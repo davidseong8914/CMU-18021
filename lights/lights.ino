1987fe0c85d2cd529478b8a86e46d127aeef404b
Seungje <davidseong8914@gmail.com>
2024-09-14 16:06:29 -0400

    add 3V 5V switch

--- lights/lights.ino
+++ lights/lights.ino
@@ -1,14 +1,18 @@
 void setup() {
 Serial.begin(9600);
 pinMode(2, OUTPUT);
+// pinMode(3, OUTPUT);
 }
 
 void loop() {
+
     digitalWrite(2, HIGH);
-    delay(100);
-    digitalWrite(2, High);
-    delay(100);
+    delay(500);
+    digitalWrite(2, HIGH);
+    delay(500);
+
     digitalWrite(2, LOW);
-    delay(100);
+    delay(500);
     digitalWrite(2, LOW);
+    delay(500);
 }
