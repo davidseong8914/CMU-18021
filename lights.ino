2dd73ae8e95ecd29f6a15ecd9896bfa43658716e
Seungje <davidseong8914@gmail.com>
2024-09-14 15:59:03 -0400

    initial commit

---
+++ lights.ino
@@ -0,0 +1,14 @@
+void setup() {
+Serial.begin(9600);
+pinMode(2, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(2, HIGH);
+    delay(100);
+    digitalWrite(2, High);
+    delay(100);
+    digitalWrite(2, LOW);
+    delay(100);
+    digitalWrite(2, LOW);
+}
