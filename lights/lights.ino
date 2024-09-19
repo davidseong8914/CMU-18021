26e56f8dce155c611ac280f4f2c0ff1c15f42244
Seungje <davidseong8914@gmail.com>
2024-09-19 09:15:12 -0400

    add print statements

--- lights/lights.ino
+++ lights/lights.ino
@@ -6,11 +6,17 @@ pinMode(2, OUTPUT);
 
 void loop() {
 
+    // Code to turn on and off the lights
+
+    // High Voltage
+    Serial.println("Arduino now outputting High Voltage: 5V");
     digitalWrite(2, HIGH);
     delay(500);
     digitalWrite(2, HIGH);
     delay(500);
 
+    // Low Voltage
+    Serial.println("Arduino now outputting Low Voltage: 3.3V");
     digitalWrite(2, LOW);
     delay(500);
     digitalWrite(2, LOW);
